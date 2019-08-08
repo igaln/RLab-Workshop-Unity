77ca1c946a5040c172e76bb19819a10ceb8a0684
igal nassima <inassima@gmail.com>
2019-08-08 15:30:57 -0400

    ok

---
+++ Assets/RandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerator : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = new Vector3(Random.Range(-50.0f, 50.0f), Random.Range(10.0f, 50.0f), Random.Range(-50.0f,50.0f));
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
